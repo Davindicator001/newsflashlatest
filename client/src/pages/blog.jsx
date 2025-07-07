72b9012c2d71ce4dcbf368c97bd4774d452e2997
Davindicator <victorakande090@gmail.com>
2025-07-07 10:44:42 +0100

    pictures

--- client/src/pages/blog.jsx
+++ client/src/pages/blog.jsx
@@ -247,7 +247,7 @@ function Blog() {
         {/* Featured Image its unique so i guess its not gonna match the post */}
         <div className="relative h-48 z-1 bg-slate-200 rounded-lg overflow-hidden mb-6">
           <img 
-            src={`https://picsum.photos/seed/${getBlogId()}/1200/400`} 
+            src={`static.photos/1200x400/${getBlogId()}`} 
             alt="Blog feature" 
             className="w-full h-full object-cover mix-blend-multiply"
           />
